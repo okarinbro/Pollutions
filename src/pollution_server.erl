8df87b534b41051242347b099377613c9d99c648
okarinbro <prz.jab98@gmail.com>
2019-05-05 14:27:11 +0200

    new functions templates

--- src/pollution_server.erl
+++ src/pollution_server.erl
@@ -10,7 +10,8 @@
 -author("Acer").
 
 %% API
--export([start/0, stop/0, addStation/2, addValue/4, removeValue/3]).
+-export([start/0, stop/0, addStation/2, addValue/4, removeValue/3,
+  getOneValue/3, getStationMean/2, getDailyMean/2, getMinMaxValue/3]).
 
 start() ->
   ServerPID = spawn(fun() -> init() end),
@@ -26,17 +27,34 @@ init() ->
 loop(MonitorState) ->
   receive
     terminate -> ok;
-    {request, PID, {addStation, Name, Coords}} -> NextState = pollution:addStation(Name, Coords, MonitorState),
-      PID ! {reply, added},
+    {request, PID, {addStation, Name, Coords}} ->
+      NextState = pollution:addStation(Name, Coords, MonitorState),
+      PID ! {reply, NextState},
       loop(NextState);
     {request, PID, {addValue, Key, Date, Type, Value}} ->
       NextState = pollution:addValue(Key, Date, Type, Value, MonitorState),
-      PID ! {reply, value_added},
+      PID ! {reply, NextState},
       loop(NextState);
     {request, PID, {removeValue, Key, Date, Type}} ->
       NextState = pollution:removeValue(Key, Date, Type, MonitorState),
-      PID ! {reply, removed},
-      loop(NextState)
+      PID ! {reply, NextState},
+      loop(NextState);
+    {request, PID, {getOneValue, Key, Date, Type}} ->
+      Val = pollution:getOneValue(Key, Date, Type, MonitorState),
+      PID ! {reply, Val},
+      loop(MonitorState);
+    {request, PID, {getStationMean, Key, Type}} ->
+      Val = pollution:getStationMean(Key, Type, MonitorState),
+      PID ! {reply, Val},
+      loop(MonitorState);
+    {request, PID, {getDailyMean, Day, Type}} ->
+      Val = pollution:getDailyMean(Day, Type, MonitorState),
+      PID ! {reply, Val},
+      loop(MonitorState);
+    {request, PID, {getMinMaxValue, Coords, Day, Type}} ->
+      Val = pollution:getMinMaxValue(Coords, Day, Type, MonitorState),
+      PID ! {reply, Val},
+      loop(MonitorState)
   end.
 
 call(Msg) ->
@@ -54,3 +72,15 @@ addValue(Key, Date, Type, Value) ->
 
 removeValue(Key, Date, Type) ->
   call({removeValue, Key, Date, Type}).
+
+getOneValue(_Arg0, _Arg1, _Arg2) ->
+  erlang:error(not_implemented).
+
+getStationMean(_Arg0, _Arg1) ->
+  erlang:error(not_implemented).
+
+getDailyMean(_Arg0, _Arg1) ->
+  erlang:error(not_implemented).
+
+getMinMaxValue(_Arg0, _Arg1, _Arg2) ->
+  erlang:error(not_implemented).
